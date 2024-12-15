8a2b8b9802651a73fbf169d540264c9434dcf086
Gabriel <gabriel_azevedocs@outlook.com>
2024-12-15 17:53:00 -0300

    up2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
